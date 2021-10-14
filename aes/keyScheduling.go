80b9fce88f871e10a727b81eb8b3afd270ccc84a
Noman-Aziz <aziznoman634@gmail.com>
2021-10-14 12:27:05 +0500

    Done Key Expansion

--- aes/keyScheduling.go
+++ aes/keyScheduling.go
@@ -1,6 +1,151 @@
 package aes
 
-func GenerateRoundKeys(prevRoundKey []byte) []byte {
-	var newRoundKey []byte
+import (
+	"strconv"
+)
 
+func doXOR(a [4]byte, b [4]byte) [4]byte {
+	var c [4]byte
+
+	for i := 0; i < 4; i++ {
+		c[i] = a[i] ^ b[i]
+	}
+
+	return c
+}
+
+func hex2int(l string) int {
+	switch l {
+	case "0":
+		return 0
+	case "1":
+		return 1
+	case "2":
+		return 2
+	case "3":
+		return 3
+	case "4":
+		return 4
+	case "5":
+		return 5
+	case "6":
+		return 6
+	case "7":
+		return 7
+	case "8":
+		return 8
+	case "9":
+		return 9
+	case "a":
+		return 10
+	case "b":
+		return 11
+	case "c":
+		return 12
+	case "d":
+		return 13
+	case "e":
+		return 14
+	case "f":
+		return 15
+	}
+	return 0
+}
+
+func CircularByteLeftShift(w [4]byte) [4]byte {
+	var c [4]byte = w
+
+	var temp byte = c[0]
+	for i := 0; i < 3; i++ {
+		c[i] = c[i+1]
+	}
+	c[3] = temp
+
+	return c
+}
+
+func ByteSubsitution(w [4]byte) [4]byte {
+
+	var c [4]byte = w
+
+	for i := 0; i < 4; i++ {
+		//Decimal to Hex Equivilant
+		hexa := strconv.FormatInt(int64(c[i]), 16)
+
+		var firstIndex int = hex2int(string(hexa[0]))
+		var secondIndex int
+		if len(hexa) > 1 {
+			secondIndex = hex2int(string(hexa[1]))
+		} else {
+			secondIndex = 0
+		}
+
+		//row * length + col
+		c[i] = sbox[firstIndex*16+secondIndex]
+	}
+
+	return c
+}
+
+func AddingRoundConstant(w [4]byte, round int) [4]byte {
+
+	var c [4]byte = w
+
+	c[0] = c[0] ^ RoundConstants[round]
+	c[1] = c[1] ^ 0x00
+	c[2] = c[2] ^ 0x00
+	c[3] = c[3] ^ 0x00
+
+	return c
+}
+
+func GW(w [4]byte, round int) [4]byte {
+	var g [4]byte = w
+
+	g = CircularByteLeftShift(g)
+	g = ByteSubsitution(g)
+	g = AddingRoundConstant(g, round)
+
+	return g
+}
+
+func GenerateRoundKeys(prevRoundKey []byte, round int, columnSize int, totalSize int) []byte {
+	var newRoundKey = make([]byte, totalSize)
+
+	//Seperating W Values
+	var w = make([][4]byte, columnSize)
+
+	var k int = 0
+	for i := 0; i < columnSize; i++ {
+		for j := 0; j < 4; j++ {
+			w[i][j] = prevRoundKey[k]
+			k++
+		}
+	}
+
+	var gw3 [4]byte = GW(w[3], round)
+
+	var newW = make([][4]byte, columnSize)
+
+	//Filling New W Values
+	newW[0] = doXOR(w[0], gw3)
+	index := 0
+
+	for j := 0; j < 4; j++ {
+		newRoundKey[index] = newW[0][j]
+		index++
+	}
+
+	prevW := 1
+	for i := 1; i < columnSize; i++ {
+		newW[i] = doXOR(newW[i-1], w[prevW])
+		prevW++
+
+		for j := 0; j < 4; j++ {
+			newRoundKey[index] = newW[i][j]
+			index++
+		}
+	}
+
+	return newRoundKey
 }
--- main.go
+++ main.go
@@ -2,20 +2,53 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
+
+	"github.com/noman-aziz/AES/aes"
 )
+
+type Key struct {
+	Rounds     int //10 in 128 bits, 12 in 192 bits,14 in 256 bits
+	ColumnSize int //4 in 128 bits, 6 in 192 bits, 8 in 256 bits
+	TotalSize  int //ColumnSize * 4
+	RoundKeys  [][]byte
+}
 
 func main() {
 
+	//Custom Input Reader
 	cin := bufio.NewScanner(os.Stdin)
-	cin.Scan()
 
-	var arr []byte = cin.Bytes()
+	//Initial Variables
+	var keys Key
+	keys.Rounds = 10
+	keys.ColumnSize = 4
+	keys.TotalSize = 4 * keys.ColumnSize
 
-	for _, v := range arr {
-		hexa := strconv.FormatInt(int64(v), 16)
-		print(hexa, " ")
+	//Fixing Sizes for Dynamic Array
+	keys.RoundKeys = make([][]byte, keys.Rounds+1)
+	for i := 0; i < keys.Rounds; i++ {
+		keys.RoundKeys[i] = make([]byte, keys.TotalSize)
 	}
 
+	//Taking Key Input
+	fmt.Printf("Enter cipher key : ")
+	cin.Scan()
+
+	//Read initial key
+	keys.RoundKeys[0] = cin.Bytes()
+
+	for i := 0; i < keys.Rounds; i++ {
+		keys.RoundKeys[i+1] = aes.GenerateRoundKeys(keys.RoundKeys[i], i, keys.ColumnSize, keys.TotalSize)
+	}
+
+	for i := 0; i <= keys.Rounds; i++ {
+		fmt.Print("Round ", i, " : ")
+		for j := 0; j < keys.TotalSize; j++ {
+			fmt.Print(strconv.FormatInt(int64(keys.RoundKeys[i][j]), 16), " ")
+		}
+		fmt.Println()
+	}
 }
